c354bbb39da4f5632d0048733fb93bf061be349d
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2015-02-01 14:57:07 +0900

    refactor

--- client/app/address/address.controller.spec.js
+++ client/app/address/address.controller.spec.js
@@ -1,33 +1,25 @@
 'use strict';
 
 describe('Controller: AddressCtrl', function () {
-  var $compile, $rootScope;
 
   // load the controller's module
   beforeEach(module('AngularJsTestson'));
 
   var AddressCtrl, scope;
+  var $compile, $rootScope;
 
   //// Initialize the controller and a mock scope
-  //beforeEach(inject(function ($controller, $rootScope) {
-  //  scope = $rootScope.$new();
-  //  AddressCtrl = $controller('AddressCtrl', {
-  //    $scope: scope
-  //  });
-  //}));
-  beforeEach(inject(function(_$compile_, _$rootScope_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
+  beforeEach(inject(function ($controller, _$rootScope_) {
     scope = _$rootScope_.$new();
-    AddressCtrl = _$compile_('AddressCtrl', {
+    AddressCtrl = $controller('AddressCtrl', {
       $scope: scope
     });
+    $compile = $controller;
+    $rootScope = _$rootScope_;
   }));
 
   it('should has $scope.isLoading value to be false', function () {
     expect(scope.isLoading).toBe(false);
-    //expect(1).toEqual(1);
   });
 
   //it('should has $scope.payment is correct', function () {
@@ -36,13 +28,10 @@ describe('Controller: AddressCtrl', function () {
   //});
 
   it('testing isTelephone directive', function () {
-    // Compile a piece of HTML containing the directive
-    var element = $compile("<input ng-model=\"order.tel\" type=\"tel\" is-telephone value=\"あああ\" />")($rootScope);
-    // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
-    $rootScope.$digest();
-    // Check that the compiled element contains the templated content
-    console.log(element)
-    console.log('$rootScope: ', scope.purch_form)
+    //$rootScope.$digest();
+    //var node = $compile("<input ng-model=\"order.tel\" type=\"tel\" is-telephone />")($rootScope);
+    //$rootScope.$digest();
+    //console.log('scope: ', scope)
     //expect(1).toEqual(1);
   });
 });
